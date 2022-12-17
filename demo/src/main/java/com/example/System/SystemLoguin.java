4207f722b328ecaae3fcf20bcb2aed023d8ebad7
PEDROHCA18 <108296717+ericlindo@users.noreply.github.com>
2022-12-17 04:38:51 -0300

    testando login

--- demo/src/main/java/com/example/System/SystemLoguin.java
+++ demo/src/main/java/com/example/System/SystemLoguin.java
@@ -29,9 +29,9 @@ public class SystemLoguin  implements ActionListener {
 
 
         JButton jbutton = new JButton("ENTRAR");
-        jbutton.setBounds(280, 300, 250, 70);
+        jbutton.setBounds(280, 350, 250, 70);
         jbutton.setFont(new Font("Arial", Font.BOLD, 30));
-        jbutton.setForeground(new Color(200, 72, 72));
+        jbutton.setForeground(new Color(70, 103, 200));
         jbutton.setBackground(new Color(12, 15, 10));
 
         jframe2.add(jbutton);
@@ -40,21 +40,50 @@ public class SystemLoguin  implements ActionListener {
 
 
         n1 = new JTextField(10);
-        n1.setBounds(100, 100, 100, 100);
+        n1.setBounds(280, 150, 250, 50);
         n1.setFont(new Font("Arial", Font.BOLD, 30));
-        n1.setForeground(new Color(200, 72, 70));
-        n1.setBackground(new Color(12, 15, 10));
+        n1.setForeground(new Color(70, 103, 200));
+        n1.setBackground(new Color(255, 255, 255));
 
         n2 = new JTextField(10);
-        n2.setBounds(100, 300, 100, 100);
+        n2.setBounds(280, 250, 250, 50);
         n2.setFont(new Font("Arial", Font.BOLD, 30));
-        n2.setForeground(new Color(200, 72, 70));
-        n2.setBackground(new Color(12, 15, 10));
-        n2.setText("texto");
+        n2.setForeground(new Color(70, 103, 200));
+        n2.setBackground(new Color(255, 255, 255));
 
         jframe2.add(n1);
         jframe2.add(n2);
 
+        JLabel jLabel = new JLabel("USUARIO:");
+        jLabel.setBounds(280, 100, 280, 50);
+        jLabel.setFont(new Font("Arial", Font.BOLD, 30));
+        jLabel.setForeground(new Color(70,103,200));
+        jLabel.setBackground(new Color(255, 255, 255));
+
+        JLabel jLabel2 = new JLabel("SENHA:");
+        jLabel2.setBounds(280, 200, 280, 50);
+        jLabel2.setFont(new Font("Arial", Font.BOLD, 30));
+        jLabel2.setForeground(new Color(70,103,200));
+        jLabel2.setBackground(new Color(255, 255, 255));
+
+        JLabel jLabel3 = new JLabel("LOGIN");
+        jLabel3.setBounds(350, 25, 250, 70);
+        jLabel3.setFont(new Font("Helvetica", Font.BOLD, 30));
+        jLabel3.setForeground(new Color(200, 70, 85));
+        jLabel3.setBackground(new Color(255, 255, 255));
+
+        JButton jbutton2 = new JButton("CADASTRAR");
+        jbutton2.setBounds(20, 420, 160, 30);
+        jbutton2.setFont(new Font("Arial", Font.BOLD, 20));
+        jbutton2.setForeground(new Color(127, 166, 187, 255));
+        jbutton2.setBackground(new Color(255, 255, 255, 255));
+
+        jframe2.add(jbutton);
+        jframe2.add(jbutton2);
+        jframe2.add(jLabel);
+        jframe2.add(jLabel2);
+        jframe2.add(jLabel3);
+        jframe2.add(jbutton2);
         jbutton.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
@@ -65,15 +94,18 @@ public class SystemLoguin  implements ActionListener {
                 }
             }
         });
+
+
+        jbutton2.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+
+            }
+        });
         jframe2.setVisible(true);
 
 
     }
-
-    public void test(ActionEvent e) {
-        JOptionPane.showMessageDialog(null, "ERIC");
-    }
-
 
 
     public void login() throws SQLException {
@@ -91,6 +123,7 @@ public class SystemLoguin  implements ActionListener {
 JOptionPane.showMessageDialog(null,"erro ao autenticar"+e.getMessage());
         }
     }
+
 
     @Override
     public void actionPerformed(ActionEvent e) {
